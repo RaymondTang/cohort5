4aa5fe7dd08040ee8dd9c45e5f65be16c47e5917
Henry Fong <foongsy@gmail.com>
2025-11-01 13:35:03 +0800

    Implement Langfuse client authentication and integrate Pydantic AI agent; update dependencies in pyproject.toml

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from cohort5!")
+from pydantic_ai import Agent
+from langfuse import get_client
+from dotenv import load_dotenv
+load_dotenv()
+ 
+langfuse = get_client()
+ 
+# Verify connection
+if langfuse.auth_check():
+    print("Langfuse client is authenticated and ready!")
+    Agent.instrument_all()
+else:
+    print("Authentication failed. Please check your credentials and host.")
 
+agent = Agent('openrouter:google/gemini-2.0-flash-lite-001')
 
-if __name__ == "__main__":
-    main()
+prompt = langfuse.get_prompt("firstprompt")
+
+r = agent.run_sync(prompt.compile(language="Traditional Chinese", position="Delivery Manager"))
+
+print(r.output)
